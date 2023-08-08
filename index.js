488d9aa67b53d724099e768d5deff857c312b431
MrWiner <moeinbahal@live.com>
2023-08-08 03:47:19 +0330

    using playwright instead of puppeteer

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 require('dotenv').config();
 const nintendoSwitch = require('./listMakers/nintendoSwitch');
+const ExcelJS = require('exceljs');
 
 app.use(bodyParser.json());
 app.use(cors());
@@ -17,7 +18,34 @@ const list = require('./routes/api/list');
 app.use('/api/list', list);
 
 async function main() {
-  const a = await nintendoSwitch.getNintendoGames(2);
+  const a = await nintendoSwitch.nintendoWebsite('World War: Combat Guardian');
   console.log(a);
+  // const workbook = new ExcelJS.Workbook();
+  // const worksheet = workbook.addWorksheet('Sheet1');
+  // const columns = [
+  //   { header: 'Title', key: 'title', width: 15 },
+  //   { header: 'Release Date', key: 'releaseDate', width: 15 },
+  //   { header: 'Metacritic Link', key: 'metacriticLink', width: 20 },
+  //   { header: 'Publisher', key: 'publisher', width: 25 },
+  //   { header: 'also on', key: 'alsoOnPlatforms', width: 30 },
+  //   { header: 'Nintendo Link', key: 'nintendoLink', width: 20 },
+  //   { header: 'Price', key: 'regularPrice', width: 5 },
+  // ];
+  // worksheet.columns = columns;
+
+  // a.forEach((rowData) => {
+  //   worksheet.addRow(rowData);
+  // });
+
+  // const excelFilePath =
+  //   '/Users/moeinzavar/Desktop/upcomingVideoGames/excel/data2.xlsx';
+  // workbook.xlsx
+  //   .writeFile(excelFilePath)
+  //   .then(() => {
+  //     console.log(`Excel file "${excelFilePath}" created successfully.`);
+  //   })
+  //   .catch((error) => {
+  //     console.error('Error creating Excel file:', error);
+  //   });
 }
 main();
